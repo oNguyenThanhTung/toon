faefa0cf6c918930641f008e5ae83582317c0823
nguyen-thanh-tungb <nguyen.thanh.tungb@framgia.com>
2017-05-21 14:36:36 +0700

    add dotenv gem

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,20 +1,47 @@
-# set path to application
-app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
-working_directory app_dir
+require 'active_support/core_ext/string'
 
+rails_env = ENV["RAILS_ENV"] || "production"
+num_workers = ENV["NUM_UNICORN_WORKERS"]
+worker_processes (num_workers ? num_workers.to_i : 3)
+app_name = ENV["REPO_URL"].split("/").last.gsub(".git","").underscore.camelize
 
-# Set unicorn options
-worker_processes 2
+app_directory = "/usr/local/rails_apps/#{app_name}/current"
+working_directory app_directory # available in 0.94.0+
+
+listen "#{app_directory}/tmp/sockets/unicorn.sock", backlog: 128
+
+timeout 3000
+
+pid "#{app_directory}/tmp/pids/unicorn.pid"
+
+stderr_path "#{app_directory}/log/unicorn_#{rails_env}.log"
+stdout_path "#{app_directory}/log/unicorn_#{rails_env}.log"
+
 preload_app true
-timeout 30
+GC.respond_to?(:copy_on_write_friendly=) and
+  GC.copy_on_write_friendly = true
 
-# Set up socket location
-listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+before_exec do |server|
+  ENV["BUNDLE_GEMFILE"] = "#{app_directory}/Gemfile"
+end
 
-# Logging
-stderr_path "#{shared_dir}/log/unicorn.stderr.log"
-stdout_path "#{shared_dir}/log/unicorn.stdout.log"
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
 
-# Set master PID location
-pid "#{shared_dir}/pids/unicorn.pid"
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+
+  sleep 1
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
