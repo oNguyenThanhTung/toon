8158b71f8bef4f2a1b71a7f1fddb7aabeed1fec5
nguyen-thanh-tungb <nguyen.thanh.tungb@framgia.com>
2017-05-21 13:13:16 +0700

    comment out failed test case

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe User do
 			expect(User.new.add(2, 3)).to eq 5	
 	  end
 	end
-  context "add 2 normal int" do
-	  it "should be true result" do
-			expect(User.new.add(2, 3)).to eq 4	
-	  end
-	end
+ #  context "add 2 normal int" do
+	#   it "should be true result" do
+	# 		expect(User.new.add(2, 3)).to eq 4	
+	#   end
+	# end
 end
